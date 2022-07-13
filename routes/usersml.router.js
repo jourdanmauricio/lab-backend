bd79f479299b74668605e2818ec27c2a674d7ddd
Mauricio <jourdanmauricio@gmail.com>
2022-07-13 05:47:09 -0300

    add get user Ml

--- routes/usersml.router.js
+++ routes/usersml.router.js
@@ -36,7 +36,7 @@ router.get('/callback', async (req, res, next) => {
     const rta = await service.update(state, resMl);
     // return rta;
     // res.status(200);
-    res.send('OK');
+    res.send(resMl);
   } catch (error) {
     next(error);
   }
