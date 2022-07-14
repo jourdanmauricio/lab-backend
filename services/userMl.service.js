921593cabfbc429cb7e5cb46c9a1b57ecfa64a35
Mauricio <jourdanmauricio@gmail.com>
2022-07-14 03:56:29 -0300

    add get user Ml

--- services/userMl.service.js
+++ services/userMl.service.js
@@ -52,7 +52,7 @@ class UserMlService {
   async updateMl(changes) {
     const token = changes.token;
     const payload = await jwt.verify(token, config.jwtSecret);
-    const user = await service.findOne(payload.sub);
+    const user = await this.findOne(payload.sub);
 
     return user;
 
